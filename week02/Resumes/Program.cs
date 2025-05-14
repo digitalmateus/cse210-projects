697691405993a3ceea9c552fe634715ac7393b48
digitalmateus <mnunes@byupathway.edu>
2025-05-14 16:14:57 -0300

    week2 abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+      //  Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+       // job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Data Scientist";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+      //  job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Mateus Nunes";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
